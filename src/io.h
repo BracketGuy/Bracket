4acdc0e9e1f6096bd8d058febe7cb5810b5e04be
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2014-09-16 07:54:02 -0400

    Added documentation to io.h

--- src/io.h
+++ src/io.h
@@ -2,5 +2,9 @@
  *     \brief Handles command-line arguments.
  *         \param argc The number of command-line arguments.
  *             \param argv An array of command-line arguments.
- *             */
+ *          
+ *  Takes the command-line arguments passed to main and returns the
+ *  filename that we are going to process. Or, if no filename was found,
+ *  it returns NULL.  
+ */
 const char * handle_command_args(int argc, char **argv);
