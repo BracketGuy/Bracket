5bfcd241994d2a4000a74ab1bd94a4df0b86e7f7
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2014-09-15 20:00:23 -0400

    Update main.c
    Added comment.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ This is the main file for the project.
 #include <stdio.h>
 #include "io.h"
 
-const char *filename = { 0 };
+const char *filename = { 0 }; /* Name of file to use */
 
 int main (int argc, char *argv[])
 {
