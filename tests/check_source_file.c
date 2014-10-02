8cce7243743a46f5aee15627f4a6ed4f486d08d5
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2014-10-02 09:38:37 -0400

    Added a test

--- tests/check_source_file.c
+++ tests/check_source_file.c
@@ -1,7 +1,13 @@
 #include <check.h>
 #include "../src/source_file.h"
 
+START_TEST(test_sanity)
+{
+    ck_assert_int_eq(1,1);
+}
+END_TEST
+
 int main(int argc, char *argv[])
 {
-    /* Nothing to see here... */
+    return 0;     
 }
