286e54a8801d895d083c4d79bcbd135f22595519
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2014-10-11 21:19:23 -0400

    Now actually points to function we're testing.

--- tests/check_source_file_create.c
+++ tests/check_source_file_create.c
@@ -2,13 +2,8 @@
 #include "inter_unit.h"
 #include "../src/source_file.h"
 
-int test_create_source_file(void)
-{
-    return 0;
-}
-
 int main(void)
 {
-    int number_failed = test_create_source_file();
-    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+    int lines = create_source_file(1);
+    return (lines == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
